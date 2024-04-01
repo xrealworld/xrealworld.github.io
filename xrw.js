85be012834915a33dab422eed8525d16bb15b4c4
Vinod Pahuja <vinod.pahuja@gmail.com>
2024-04-01 21:16:19 +0530

    Initial import

---
+++ xrw.js
@@ -0,0 +1,10 @@
+
+var MAP_BASE = '';
+//var MAP_BASE = 'http://localhost:8080';
+var mapId = '/xrw';
+var map = mmw.map;
+
+//mmw._common.setType('vector');
+map.flyTo({center : [77.43,28.701], zoom: 15});
+
+xrnav.start();
